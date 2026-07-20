04d4b6b18c2d5f08be373d8307398653f6ab3d20
wcui2016 <cuiwei386@163.com>
2026-07-20 12:42:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,3 @@ if (navToggle && siteNav) {
   });
 }
 
-const contactForm = document.querySelector('.contact-form');
-if (contactForm) {
-  contactForm.addEventListener('submit', (event) => {
-    const button = contactForm.querySelector('button');
-    if (button) {
-      button.textContent = '已收到您的咨询';
-    }
-  });
-}
